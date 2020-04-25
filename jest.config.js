f059f8ae3b09b43d1e489276fc5368629e52a58f
Alexander Nedomansky <anedomansky@googlemail.com>
2020-04-25 11:33:33 +0200

    Added setupFilesAfterEnv to jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    setupFilesAfterEnv: [
+        '<rootDir>/src/setupTests.ts',
+    ],
     // needed to access the @testing-library/jest-dom methods like expect(container).toBeInTheDocument();
     testEnvironment: 'jest-environment-jsdom-fourteen',
 };
