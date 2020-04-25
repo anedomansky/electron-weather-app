6cd1688864fc0df8f46db32cb39b15361572ba40
Alexander Nedomansky <anedomansky@googlemail.com>
2020-04-25 11:06:22 +0200

    Updated the unit testing setup. Added .gitattributes file.

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,4 @@
 module.exports = {
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json"
-    ],
-    "setupFiles": [
-        "<rootDir>/test/setupTests.ts"
-    ],
-    "moduleNameMapper": {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/fileMock.ts",
-        "\\.(css|pcss|scss)$": "<rootDir>/test/styleMock.ts"
-    },
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-        "**src/**/*.{ts,tsx}"
-    ]
-}
+    // needed to access the @testing-library/jest-dom methods like expect(container).toBeInTheDocument();
+    testEnvironment: 'jest-environment-jsdom-fourteen',
+};
--- main.ts
+++ main.ts
@@ -10,14 +10,14 @@ const PORT = process.env.PORT || 4000;
 
 let window: BrowserWindow | null;
 
-const installExtensions = async () => {
+const installExtensions = async (): Promise<unknown> => {
     const installer = require('electron-devtools-installer');
     const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
     const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
 
     return Promise.all(
         extensions.map((name) => installer.default(installer[name], forceDownload)),
-    ).catch(console.log); // eslint-disable-line no-console
+    ).catch(console.log);
 };
 
 const createWindow = async (): Promise<void> => {
@@ -26,7 +26,7 @@ const createWindow = async (): Promise<void> => {
     }
 
     window = new BrowserWindow({
-        width: 800,
+        width: 900,
         height: 600,
         webPreferences: {
             nodeIntegration: true,
